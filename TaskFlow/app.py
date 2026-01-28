2e009a516bbedbb900cdbc9a1336c178688e323d
aasim144 <aasimshaikh1404@gmail.com>
2026-01-28 16:53:10 +0530

    Update app.py

--- TaskFlow/app.py
+++ TaskFlow/app.py
@@ -14,7 +14,7 @@ def get_db():
 # -------- HOME --------
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 # ---------------- REGISTER ----------------
 @app.route("/register", methods=["GET", "POST"])
@@ -203,3 +203,4 @@ def toggle_complete():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
